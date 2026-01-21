9d23036e100beec24111fed3f84869b12c27920d
Kevin Segaud <kevin@kleak.dev>
2026-01-21 16:07:31 +0100

    refactor: update `counter_final` export to use conditional platform-specific implementations.

--- packages/spark/example/lib/components/counter_final/counter_final.dart
+++ packages/spark/example/lib/components/counter_final/counter_final.dart
@@ -1,1 +1,3 @@
-export 'counter_final_base.impl.dart';
+export 'counter_final_base.dart'
+    if (dart.library.io) 'counter_final_base.impl.dart'
+    if (dart.library.html) 'counter_final_base.impl.dart';
