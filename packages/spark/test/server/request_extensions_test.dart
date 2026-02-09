532b6776f78cab16d37eaf6dd9e95ecfa6d00e7b
kevin-sakemaer <5434752+kevin-sakemaer@users.noreply.github.com>
2026-02-09 08:17:55 +0000

    test: improve coverage for server request extensions
    - Rename `packages/spark/test/server/request_extensions_test.dart` to `packages/spark/test/server/request_ip_extension_test.dart`.
    - Create new `packages/spark/test/server/request_extensions_test.dart` with tests for `RequestSet`, `RequestGet`, and `RequestGetPathParameter`.

--- packages/spark/test/server/request_extensions_test.dart
+++ packages/spark/test/server/request_extensions_test.dart
@@ -1,71 +1,101 @@
 @TestOn('vm')
 library;
 
-import 'dart:io';
-
 import 'package:shelf/shelf.dart';
-import 'package:spark_framework/src/server/request_ip_extension.dart';
+import 'package:spark_framework/src/server/request_extensions.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('RequestClientIp', () {
-    test('returns ip from x-forwarded-for header', () {
-      final request = Request(
-        'GET',
-        Uri.parse('http://localhost/'),
-        headers: {'x-forwarded-for': '1.2.3.4, 5.6.7.8'},
-      );
-      expect(request.clientIp, '1.2.3.4');
-    });
-
-    test('returns null if x-forwarded-for is empty', () {
-      final request = Request(
-        'GET',
-        Uri.parse('http://localhost/'),
-        headers: {'x-forwarded-for': ''},
-      );
-      expect(request.clientIp, null);
-    });
-
-    test('returns ip from connection info if header is missing', () {
-      final connectionInfo = _MockHttpConnectionInfo(
-        InternetAddress('9.8.7.6', type: InternetAddressType.IPv4),
-      );
-
-      final request = Request(
-        'GET',
-        Uri.parse('http://localhost/'),
-        context: {'shelf.io.connection_info': connectionInfo},
-      );
-      expect(request.clientIp, '9.8.7.6');
-    });
-
-    test('prioritizes header over connection info', () {
-      final connectionInfo = _MockHttpConnectionInfo(
-        InternetAddress('9.8.7.6', type: InternetAddressType.IPv4),
-      );
-      final request = Request(
-        'GET',
-        Uri.parse('http://localhost/'),
-        headers: {'x-forwarded-for': '1.2.3.4'},
-        context: {'shelf.io.connection_info': connectionInfo},
-      );
-      expect(request.clientIp, '1.2.3.4');
-    });
-
-    test('returns null if both missing', () {
+  group('RequestSet and RequestGet', () {
+    test('can set and get a value of type T', () {
       final request = Request('GET', Uri.parse('http://localhost/'));
-      expect(request.clientIp, null);
+      final newRequest = request.set<String>(() => 'test_value');
+
+      expect(newRequest.get<String>(), 'test_value');
+    });
+
+    test('value is initialized lazily', () {
+      var callCount = 0;
+      final request = Request('GET', Uri.parse('http://localhost/'));
+      final newRequest = request.set<String>(() {
+        callCount++;
+        return 'test_value';
+      });
+
+      expect(callCount, 0); // Not called yet
+      expect(newRequest.get<String>(), 'test_value');
+      expect(callCount, 1); // Called once
+    });
+
+    test('can set and get multiple values of different types', () {
+      final request = Request('GET', Uri.parse('http://localhost/'));
+      final newRequest = request
+          .set<String>(() => 'string_value')
+          .set<int>(() => 42);
+
+      expect(newRequest.get<String>(), 'string_value');
+      expect(newRequest.get<int>(), 42);
+    });
+
+    test('throws StateError when get<T> is called without a provider', () {
+      final request = Request('GET', Uri.parse('http://localhost/'));
+      expect(
+        () => request.get<String>(),
+        throwsA(
+          isA<StateError>().having(
+            (e) => e.message,
+            'message',
+            contains('request.get<String>() called with a request context that does not contain a String'),
+          ),
+        ),
+      );
     });
   });
-}
 
-class _MockHttpConnectionInfo implements HttpConnectionInfo {
-  @override
-  final InternetAddress remoteAddress;
+  group('RequestGetPathParameter', () {
+    test('can get an existing path parameter', () {
+      final request = Request(
+        'GET',
+        Uri.parse('http://localhost/'),
+        context: {
+          'shelf_router/params': {'id': '123'},
+        },
+      );
+      expect(request.getPathParameter('id'), '123');
+    });
 
-  _MockHttpConnectionInfo(this.remoteAddress);
+    test('throws StateError when path parameter is missing', () {
+      final request = Request(
+        'GET',
+        Uri.parse('http://localhost/'),
+        context: {
+          'shelf_router/params': <String, String>{},
+        },
+      );
+      expect(
+        () => request.getPathParameter('missing_id'),
+        throwsA(
+          isA<StateError>().having(
+            (e) => e.message,
+            'message',
+            contains('request.getPathParameter(missing_id) called with a request context that does not contain a value'),
+          ),
+        ),
+      );
+    });
 
-  @override
-  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+     test('throws StateError when shelf_router/params context is missing', () {
+      final request = Request('GET', Uri.parse('http://localhost/'));
+      expect(
+        () => request.getPathParameter('missing_id'),
+        throwsA(
+          isA<StateError>().having(
+            (e) => e.message,
+            'message',
+            contains('request.getPathParameter(missing_id) called with a request context that does not contain a value'),
+          ),
+        ),
+      );
+    });
+  });
 }
