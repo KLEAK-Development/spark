d8eb296b7164ecca7af6010ba8e146cc19f7635f
kevin-sakemaer <5434752+kevin-sakemaer@users.noreply.github.com>
2026-02-09 08:17:55 +0000

    test: improve coverage for server request extensions
    - Rename `packages/spark/test/server/request_extensions_test.dart` to `packages/spark/test/server/request_ip_extension_test.dart`.
    - Create new `packages/spark/test/server/request_extensions_test.dart` with tests for `RequestSet`, `RequestGet`, and `RequestGetPathParameter`.

---
+++ packages/spark/test/server/request_ip_extension_test.dart
@@ -0,0 +1,71 @@
+@TestOn('vm')
+library;
+
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:spark_framework/src/server/request_ip_extension.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('RequestClientIp', () {
+    test('returns ip from x-forwarded-for header', () {
+      final request = Request(
+        'GET',
+        Uri.parse('http://localhost/'),
+        headers: {'x-forwarded-for': '1.2.3.4, 5.6.7.8'},
+      );
+      expect(request.clientIp, '1.2.3.4');
+    });
+
+    test('returns null if x-forwarded-for is empty', () {
+      final request = Request(
+        'GET',
+        Uri.parse('http://localhost/'),
+        headers: {'x-forwarded-for': ''},
+      );
+      expect(request.clientIp, null);
+    });
+
+    test('returns ip from connection info if header is missing', () {
+      final connectionInfo = _MockHttpConnectionInfo(
+        InternetAddress('9.8.7.6', type: InternetAddressType.IPv4),
+      );
+
+      final request = Request(
+        'GET',
+        Uri.parse('http://localhost/'),
+        context: {'shelf.io.connection_info': connectionInfo},
+      );
+      expect(request.clientIp, '9.8.7.6');
+    });
+
+    test('prioritizes header over connection info', () {
+      final connectionInfo = _MockHttpConnectionInfo(
+        InternetAddress('9.8.7.6', type: InternetAddressType.IPv4),
+      );
+      final request = Request(
+        'GET',
+        Uri.parse('http://localhost/'),
+        headers: {'x-forwarded-for': '1.2.3.4'},
+        context: {'shelf.io.connection_info': connectionInfo},
+      );
+      expect(request.clientIp, '1.2.3.4');
+    });
+
+    test('returns null if both missing', () {
+      final request = Request('GET', Uri.parse('http://localhost/'));
+      expect(request.clientIp, null);
+    });
+  });
+}
+
+class _MockHttpConnectionInfo implements HttpConnectionInfo {
+  @override
+  final InternetAddress remoteAddress;
+
+  _MockHttpConnectionInfo(this.remoteAddress);
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
