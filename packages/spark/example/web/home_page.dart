9c012897b4ce6f0df3fcc321ef6568e1757d0e36
Kevin Segaud <kevin@kleak.dev>
2026-02-12 13:56:14 +0100

    rebuild missing generated code

---
+++ packages/spark/example/web/home_page.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:spark_framework/spark.dart';
+import 'package:spark_example/components/counter_final/counter_final_base.impl.dart';
+
+void main() {
+  hydrateComponents({'counter-final': CounterFinal.new});
+}
