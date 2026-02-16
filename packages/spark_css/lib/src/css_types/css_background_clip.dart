7b6c71444127e8d0f64231c2751e3a27de2c24bd
kevin-sakemaer <5434752+kevin-sakemaer@users.noreply.github.com>
2026-02-16 17:48:47 +0000

    feat(spark_css): add CssBackgroundClip type

---
+++ packages/spark_css/lib/src/css_types/css_background_clip.dart
@@ -0,0 +1,70 @@
+import 'css_value.dart';
+
+/// CSS background-clip property values.
+sealed class CssBackgroundClip implements CssValue {
+  const CssBackgroundClip._();
+
+  /// The background extends to the outside edge of the border.
+  static const CssBackgroundClip borderBox = _CssBackgroundClipKeyword('border-box');
+
+  /// The background extends to the outside edge of the padding.
+  static const CssBackgroundClip paddingBox = _CssBackgroundClipKeyword('padding-box');
+
+  /// The background extends to the outside edge of the content.
+  static const CssBackgroundClip contentBox = _CssBackgroundClipKeyword('content-box');
+
+  /// The background is clipped to the foreground text.
+  static const CssBackgroundClip text = _CssBackgroundClipKeyword('text');
+
+  /// Multiple background-clip values for multiple background images.
+  factory CssBackgroundClip.multiple(List<CssBackgroundClip> values) = _CssBackgroundClipMultiple;
+
+  /// CSS variable reference.
+  factory CssBackgroundClip.variable(String varName) = _CssBackgroundClipVariable;
+
+  /// Raw CSS value escape hatch.
+  factory CssBackgroundClip.raw(String value) = _CssBackgroundClipRaw;
+
+  /// Global keyword (inherit, initial, unset, revert).
+  factory CssBackgroundClip.global(CssGlobal global) = _CssBackgroundClipGlobal;
+}
+
+final class _CssBackgroundClipKeyword extends CssBackgroundClip {
+  final String keyword;
+  const _CssBackgroundClipKeyword(this.keyword) : super._();
+
+  @override
+  String toCss() => keyword;
+}
+
+final class _CssBackgroundClipMultiple extends CssBackgroundClip {
+  final List<CssBackgroundClip> values;
+  const _CssBackgroundClipMultiple(this.values) : super._();
+
+  @override
+  String toCss() => values.map((e) => e.toCss()).join(', ');
+}
+
+final class _CssBackgroundClipVariable extends CssBackgroundClip {
+  final String varName;
+  const _CssBackgroundClipVariable(this.varName) : super._();
+
+  @override
+  String toCss() => 'var(--$varName)';
+}
+
+final class _CssBackgroundClipRaw extends CssBackgroundClip {
+  final String value;
+  const _CssBackgroundClipRaw(this.value) : super._();
+
+  @override
+  String toCss() => value;
+}
+
+final class _CssBackgroundClipGlobal extends CssBackgroundClip {
+  final CssGlobal global;
+  const _CssBackgroundClipGlobal(this.global) : super._();
+
+  @override
+  String toCss() => global.toCss();
+}
---
+++ packages/spark_css/test/css_background_clip_test.dart
@@ -0,0 +1,39 @@
+import 'package:test/test.dart';
+import 'package:spark_css/src/css_types/css_background_clip.dart';
+import 'package:spark_css/src/css_types/css_value.dart';
+
+void main() {
+  group('CssBackgroundClip', () {
+    test('keywords', () {
+      expect(CssBackgroundClip.borderBox.toCss(), 'border-box');
+      expect(CssBackgroundClip.paddingBox.toCss(), 'padding-box');
+      expect(CssBackgroundClip.contentBox.toCss(), 'content-box');
+      expect(CssBackgroundClip.text.toCss(), 'text');
+    });
+
+    test('multiple', () {
+      final multiple = CssBackgroundClip.multiple([
+        CssBackgroundClip.borderBox,
+        CssBackgroundClip.paddingBox,
+        CssBackgroundClip.text,
+      ]);
+      expect(multiple.toCss(), 'border-box, padding-box, text');
+    });
+
+    test('variable', () {
+      expect(CssBackgroundClip.variable('foo').toCss(), 'var(--foo)');
+    });
+
+    test('raw', () {
+      expect(CssBackgroundClip.raw('custom-value').toCss(), 'custom-value');
+    });
+
+    test('global', () {
+      expect(CssBackgroundClip.global(CssGlobal.inherit).toCss(), 'inherit');
+      expect(CssBackgroundClip.global(CssGlobal.initial).toCss(), 'initial');
+      expect(CssBackgroundClip.global(CssGlobal.unset).toCss(), 'unset');
+      expect(CssBackgroundClip.global(CssGlobal.revert).toCss(), 'revert');
+      expect(CssBackgroundClip.global(CssGlobal.revertLayer).toCss(), 'revert-layer');
+    });
+  });
+}
