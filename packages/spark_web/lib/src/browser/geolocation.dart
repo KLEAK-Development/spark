ca276170d9aa89417760ec125ff75ae9a7ea7a81
kevin-sakemaer <5434752+kevin-sakemaer@users.noreply.github.com>
2026-02-17 07:52:21 +0000

    Implement Geolocation API types in spark_web
    Added support for the Geolocation Web API with a platform-agnostic interface, a `package:web` browser implementation, and a server-side no-op implementation.
    
    - Created `packages/spark_web/lib/src/geolocation.dart` with `Geolocation` interface and data classes.
    - Created `packages/spark_web/lib/src/server/geolocation.dart` with no-op implementation.
    - Created `packages/spark_web/lib/src/browser/geolocation.dart` wrapping `package:web`.

---
+++ packages/spark_web/lib/src/browser/geolocation.dart
@@ -0,0 +1,140 @@
+/// Browser implementation of the Geolocation API wrapping `package:web`.
+library;
+
+import 'dart:js_interop';
+import 'package:web/web.dart' as web;
+
+import '../geolocation.dart' as iface;
+
+// ---------------------------------------------------------------------------
+// BrowserGeolocation
+// ---------------------------------------------------------------------------
+
+class BrowserGeolocation implements iface.Geolocation {
+  final web.Geolocation _native;
+
+  BrowserGeolocation(this._native);
+
+  @override
+  void getCurrentPosition(
+    iface.PositionCallback successCallback, [
+    iface.PositionErrorCallback? errorCallback,
+    iface.PositionOptions? options,
+  ]) {
+    _native.getCurrentPosition(
+      ((web.GeolocationPosition pos) =>
+              successCallback(BrowserGeolocationPosition(pos)))
+          .toJS,
+      errorCallback == null
+          ? null
+          : ((web.GeolocationPositionError err) =>
+                  errorCallback(BrowserGeolocationPositionError(err)))
+              .toJS,
+      _createNativeOptions(options),
+    );
+  }
+
+  @override
+  int watchPosition(
+    iface.PositionCallback successCallback, [
+    iface.PositionErrorCallback? errorCallback,
+    iface.PositionOptions? options,
+  ]) {
+    return _native.watchPosition(
+      ((web.GeolocationPosition pos) =>
+              successCallback(BrowserGeolocationPosition(pos)))
+          .toJS,
+      errorCallback == null
+          ? null
+          : ((web.GeolocationPositionError err) =>
+                  errorCallback(BrowserGeolocationPositionError(err)))
+              .toJS,
+      _createNativeOptions(options),
+    );
+  }
+
+  @override
+  void clearWatch(int watchId) => _native.clearWatch(watchId);
+
+  static web.PositionOptions? _createNativeOptions(
+    iface.PositionOptions? options,
+  ) {
+    if (options == null) return null;
+
+    final webOptions = web.PositionOptions(
+      enableHighAccuracy: options.enableHighAccuracy,
+      maximumAge: options.maximumAge,
+    );
+
+    if (options.timeout != null) {
+      webOptions.timeout = options.timeout!;
+    }
+
+    return webOptions;
+  }
+}
+
+// ---------------------------------------------------------------------------
+// BrowserGeolocationPosition
+// ---------------------------------------------------------------------------
+
+class BrowserGeolocationPosition implements iface.GeolocationPosition {
+  final web.GeolocationPosition _native;
+
+  BrowserGeolocationPosition(this._native);
+
+  @override
+  iface.GeolocationCoordinates get coords =>
+      BrowserGeolocationCoordinates(_native.coords);
+
+  @override
+  int get timestamp => _native.timestamp;
+}
+
+// ---------------------------------------------------------------------------
+// BrowserGeolocationCoordinates
+// ---------------------------------------------------------------------------
+
+class BrowserGeolocationCoordinates implements iface.GeolocationCoordinates {
+  final web.GeolocationCoordinates _native;
+
+  BrowserGeolocationCoordinates(this._native);
+
+  @override
+  double get latitude => _native.latitude;
+
+  @override
+  double get longitude => _native.longitude;
+
+  @override
+  double? get altitude => _native.altitude;
+
+  @override
+  double get accuracy => _native.accuracy;
+
+  @override
+  double? get altitudeAccuracy => _native.altitudeAccuracy;
+
+  @override
+  double? get heading => _native.heading;
+
+  @override
+  double? get speed => _native.speed;
+}
+
+// ---------------------------------------------------------------------------
+// BrowserGeolocationPositionError
+// ---------------------------------------------------------------------------
+
+class BrowserGeolocationPositionError
+    implements iface.GeolocationPositionError {
+  final web.GeolocationPositionError _native;
+
+  BrowserGeolocationPositionError(this._native);
+
+  @override
+  int get code => _native.code;
+
+  @override
+  String get message => _native.message;
+}
